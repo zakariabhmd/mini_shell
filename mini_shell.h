21d17d344fb02196ab39e5535a7c6aa52012a5d8
zakariabhmd <114684860+zakariabhmd@users.noreply.github.com>
2023-10-09 05:47:59 +0100

    Add files via upload

---
+++ mini_shell.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   mini_shell.h                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zbabahmi <zbabahmi@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/09 05:09:07 by zbabahmi          #+#    #+#             */
+/*   Updated: 2023/10/09 05:39:34 by zbabahmi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINI_SHELL_H
+# define MINI_SHELL_H
+
+#include <unistd.h>
+
+typedef struct s_savage {
+	char *command;
+	char **agrs;
+}t_savage;
+
+#endif
