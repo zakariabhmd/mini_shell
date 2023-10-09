3305ab051b9e729081aac530e47629bbe2e578f5
zakariabhmd <114684860+zakariabhmd@users.noreply.github.com>
2023-10-09 05:47:48 +0100

    Add files via upload

---
+++ main.c
@@ -0,0 +1,62 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zbabahmi <zbabahmi@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/09 05:08:55 by zbabahmi          #+#    #+#             */
+/*   Updated: 2023/10/09 05:44:48 by zbabahmi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "mini_shell.h"
+#include <stdio.h>
+#include "./libft/libft.h"
+
+void free_2d(char **str) {
+	int idx = 0;
+	while (str[idx ]){
+		free(str[idx]);
+		str[idx] = NULL;
+		idx++;
+	}
+	free(str);
+	str = NULL;
+}
+
+int main (int ac, char **av, char **env) {
+
+	t_savage *savage = ft_calloc(1, sizeof(t_savage));
+	savage[0].command = ft_strdup("grep");
+	savage[0].agrs = ft_split("grep main main.c", ' ');
+
+	int idx = 0;
+	char **holder;
+	char *holder2;
+	while (env[idx]) {
+		holder = ft_split(env[idx], '=');
+		if (ft_strncmp(holder[0], "PATH", ft_strlen(holder[0])) == 0) {
+			holder2 = ft_strdup(holder[1]);
+			free_2d(holder);
+			break;
+		}
+		free_2d(holder);
+		idx++;
+	}
+	char **path = ft_split(holder2, ':');
+	char *command;
+	idx = 0;
+	while (path[idx]) {
+		command = ft_strjoin(path[idx], "/");
+		command = ft_strjoin(command, savage[0].command);
+		// printf("%s\n"/, command);
+		if (access(command, R_OK) == 0){
+			execve(command, savage[0].agrs, env);
+			perror("fdas");
+		}
+		idx++;
+	}
+	// execv("/bin");
+	return (0);
+}
